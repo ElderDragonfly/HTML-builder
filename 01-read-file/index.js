308ac901a38b96ea68e20a6f9b3dda5aafd7467f
ElderDragonfly <ElderDragonfly@yandex.ru>
2025-01-17 21:25:07 +0300

    feat: add #1 read file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 11 */
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const readStream = fs.createReadStream(filePath, {encoding: 'utf8'});
+
+readStream.on('data', (chunk) => {
+    console.log(chunk);
+});
