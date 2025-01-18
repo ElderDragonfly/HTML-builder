50ce40af5acc66e2319411cd245db986b5fa8af7
ElderDragonfly <ElderDragonfly@yandex.ru>
2025-01-18 14:19:57 +0300

    feat: add #2 task write file

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+/*jshint esversion: 11 */
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const stream = fs.createWriteStream(filePath, { flags: 'a'}, () => {});
+
+console.log('Hello, stranger\nInput something');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    close();
+  }
+  stream.write(input);
+  console.log('Y can input something else');
+});
+
+rl.on('close', () => {
+  close();
+});
+
+function close() {
+  console.log('Goodbye, stranger');
+  stream.end();
+  process.exit(0); // Завершение процесса
+}
